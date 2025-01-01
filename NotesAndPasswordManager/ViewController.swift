a8e82aa8c38b1016a6d1ecce00605ac2a547e0e0
Alan D Medina <152593768+alandanmed@users.noreply.github.com>
2024-12-31 23:37:51 -0500

    Add files via upload

---
+++ NotesAndPasswordManager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NotesAndPasswordManager
+//
+//  Created by Alan Medina on 12/31/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '11.0'
+
+target 'NotesAndPasswordManager' do
+  use_frameworks!
+  pod 'Firebase/Auth'
+  pod 'Firebase/Firestore'
+end
